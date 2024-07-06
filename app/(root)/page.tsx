7131d14dccf7374296efbd16e30ce83f2f87b82b
NeilVillarey <neiljoey.villarey.cics@ust.edu.ph>
2024-07-06 21:04:59 +0800

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
+import RightSidebar from "@/components/RightSidebar";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalancebox from "@/components/ui/TotalBalancebox";
+
 const Home = () => {
+  const loggedIn = { firstName: "Neil", lastName:'Villarey', email: 'njvillarey@gmail.com' };
   return (
-    <div>
-      Home
-    </div>
-  )
-}
-export default Home
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome,"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalancebox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50},{currentBalance: 500.50}]} />
+    </section>
+  );
+};
+export default Home;
